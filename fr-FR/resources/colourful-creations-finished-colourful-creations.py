d863788c474bffad36d7a4fdd31f5ebfd8fbdd4d
ninaszymor <33628387+ninaszymor@users.noreply.github.com>
2018-04-23 22:31:11 +0100

    New translations colourful-creations-finished-colourful-creations.py (French)

--- fr-FR/resources/colourful-creations-finished-colourful-creations.py
+++ fr-FR/resources/colourful-creations-finished-colourful-creations.py
@@ -3,47 +3,47 @@
 from turtle import *
 from time import *
 
-# introduce dictionaries
-# use a colour picker to find and choose new colours
-colours = { 
-  'space': '#060608', 
-  'moongrey': '#BCBDEF', 
-  'verylime': '#A7E30E', 
-  'deepsea': '#226363', 
-  'reallyraspberry': '#FA057F', 
-  'gloomygrey': '#363332', 
-  'awesomeorange':  '#F37C06', 
+# introduction aux dictionnaires
+# utilise un sélecteur de couleur pour trouver et choisir de nouvelles couleurs
+couleurs = { 
+  'espace': '#060608', 
+  'grislune': '#BCBDEF', 
+  'citronvert': '#A7E30E', 
+  'profondeurs': '#226363', 
+  'superframboise': '#FA057F', 
+  'grissombre': '#363332', 
+  'toporange':  '#F37C06', 
   'coolcyan': '#4FEEF6',
-  'perfectpurple': '#6820B0',
-  'lovelylemon': '#FBF312',
+  'violetparfait': '#6820B0',
+  'citronadorable': '#FBF312',
 }
 
-screen = Screen()
-screen.setup(400, 400)
-screen.bgcolor(colours['space'])
+ecran = Screen()
+ecran.setup(420, 420)
+ecran.bgcolor(couleurs['espace'])
 
 penup()
 goto(0, 100)
-color(colours['reallyraspberry'])
+color(couleurs['superframboise'])
 style = ('Arial', 40, 'bold')
-write('HELLO', font=style, align='center')
+write('SALUT', font=style, align='center')
 right(90)
 forward(60)
-color(colours['awesomeorange'])
-write('WORLD', font=style, align='center')
+color(couleurs['toporange'])
+write('LE MONDE', font=style, align='center')
 hideturtle()
 
 sleep(3)
 
 goto(0,0)
-color(colours['moongrey'])
+color(couleurs['grislune'])
 dot(350)
 
 setheading(-90)
 penup()
 hideturtle()
 goto(0, 135)
-color(colours['verylime'])
+color(couleurs['citronvert'])
 style=('Verdana', 20, 'bold')
 write('A typical', font=style, align='center')
 forward(40)
