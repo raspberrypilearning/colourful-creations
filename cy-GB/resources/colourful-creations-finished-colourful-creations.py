db990b1b20ff3f5d2bd601046be5ef7008bc39b5
ninaszymor <33628387+ninaszymor@users.noreply.github.com>
2018-09-04 11:51:52 +0100

    New translations colourful-creations-finished-colourful-creations.py (Welsh)

--- cy-GB/resources/colourful-creations-finished-colourful-creations.py
+++ cy-GB/resources/colourful-creations-finished-colourful-creations.py
@@ -3,70 +3,70 @@
 from turtle import *
 from time import *
 
-# introduce dictionaries
-# use a colour picker to find and choose new colours
-colours = { 
-  'space': '#060608', 
-  'moongrey': '#BCBDEF', 
-  'verylime': '#A7E30E', 
-  'deepsea': '#226363', 
-  'reallyraspberry': '#FA057F', 
-  'gloomygrey': '#363332', 
-  'awesomeorange':  '#F37C06', 
-  'coolcyan': '#4FEEF6',
-  'perfectpurple': '#6820B0',
-  'lovelylemon': '#FBF312',
+# cyflwynwch eiriaduron
+# defnyddiwch ddewisydd lliw i ddarganfod a dewis lliwiau newydd
+lliwiau = { 
+  'bwlch': '#060608', 
+  'llwydlleuad': '#BCBDEF', 
+  'leimiawn': '#A7E30E', 
+  'môrdwfn': '#226363', 
+  'mafongoiawn': '#FA057F', 
+  'llwydtywyll': '#363332', 
+  'orenanhygoel':  '#F37C06', 
+  'gwyrddlascŵl': '#4FEEF6',
+  'porfforperffaith': '#6820B0',
+  'lemonhyfryd': '#FBF312',
 }
 
-screen = Screen()
-screen.setup(400, 400)
-screen.bgcolor(colours['space'])
+sgrin = Screen()
+sgrin.setup(400, 400)
+sgrin.bgcolor(lliwiau['gofod'])
 
 penup()
 goto(0, 100)
-color(colours['reallyraspberry'])
-style = ('Arial', 40, 'bold')
-write('HELLO', font=style, align='center')
+color(lliwiau['mafongoiawn'])
+arddull = ('Arial', 40, 'bold')
+write('HELO', font=arddull, align='center')
 right(90)
 forward(60)
-color(colours['awesomeorange'])
-write('WORLD', font=style, align='center')
+color(lliwiau['orenanhygoel'])
+write('BYD', font=arddull, align='center')
 hideturtle()
 
 sleep(3)
 
 goto(0,0)
-color(colours['moongrey'])
+color(lliwiau['llwydlleuad'])
 dot(350)
 
 setheading(-90)
 penup()
 hideturtle()
 goto(0, 135)
-color(colours['verylime'])
-style=('Verdana', 20, 'bold')
-write('A typical', font=style, align='center')
+color(lliwiau['leimiawn'])
+arddull=('Verdana', 20, 'bold')
+write('Mae gan', font=arddull, align='center')
 forward(40)
-color(colours['reallyraspberry'])
-write('smart phone', font=('Verdana', 25, 'bold'), align='center')
+color(lliwiau['mafongoiawn'])
+write('ffôn clyfar', font=('Verdana', 25, 'bold'), align='center')
 forward(40)
-color(colours['deepsea'])
-write('has more', font=('Verdana', 18, 'bold'), align='center')
+color(lliwiau['môrdwfn'])
+write('nodweddiadol', font=('Verdana', 18, 'bold'), align='center')
 forward(40)
-color(colours['awesomeorange'])
-write('computing power', font=('Verdana', 25, 'bold'), align='center')
+color(lliwiau['orenanhygoel'])
+write('fwy o bŵer', font=('Verdana', 25, 'bold'), align='center')
 forward(40)
-color(colours['perfectpurple'])
-write('than', font=style, align='center')
+color(lliwiau['porfforperffaith'])
+write('cyfrifiaduro nac', font=arddull, align='center')
 forward(40)
-color(colours['coolcyan'])
+color(lliwiau['gwyrddlascŵl'])
 write('Apollo 11', font=('Verdana', 25, 'bold'), align='center')
-color(colours['lovelylemon'])
+color(lliwiau['lemonhyfryd'])
 forward(40)
-write('when it landed on', font=style, align='center')
-color(colours['gloomygrey'])
+write('pan laniodd ar', font=arddull, align='center')
+color(lliwiau['llwydtywyll'])
 forward(40)
-write('the moon', font=('Verdana', 25, 'bold'), align='center')
+write('y lleuad', font=('Verdana', 25, 'bold'), align='center')
 color('white')
 forward(50)
 write('- Nancy Gibbs, 2012', font=('Arial', 14, 'normal'))
