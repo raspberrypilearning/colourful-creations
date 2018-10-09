4b4b5b1425870ead0a9665cff4df8d47ae491b53
ninaszymor <33628387+ninaszymor@users.noreply.github.com>
2018-10-09 14:41:53 +0100

    New translations colourful-creations-finished-colourful-creations.py (Portuguese, Brazilian)

--- pt-BR/resources/colourful-creations-finished-colourful-creations.py
+++ pt-BR/resources/colourful-creations-finished-colourful-creations.py
@@ -3,70 +3,70 @@
 from turtle import *
 from time import *
 
-# introduce dictionaries
-# use a colour picker to find and choose new colours
-colours = { 
-  'space': '#060608', 
-  'moongrey': '#BCBDEF', 
-  'verylime': '#A7E30E', 
-  'deepsea': '#226363', 
-  'reallyraspberry': '#FA057F', 
-  'gloomygrey': '#363332', 
-  'awesomeorange':  '#F37C06', 
-  'coolcyan': '#4FEEF6',
-  'perfectpurple': '#6820B0',
-  'lovelylemon': '#FBF312',
+# introduz dicionários
+# use um selecionador de cores para encontrar e escolher novas cores
+cores= { 
+  'espacial': '#060608', 
+  'cinzalua': '#BCBDEF', 
+  'verdelimao': '#A7E30E', 
+  'fundodomar': '#226363', 
+  'raspberry': '#FA057F', 
+  'cinzaescuro': '#363332', 
+  'laranjaforte':  '#F37C06', 
+  'azulmaneiro': '#4FEEF6',
+  'roxodahora': '#6820B0',
+  'limaobacana': '#FBF312',
 }
 
-screen = Screen()
-screen.setup(400, 400)
-screen.bgcolor(colours['space'])
+tela = Screen()
+tela.setup(400, 400)
+tela.bgcolor(cores['espacial'])
 
 penup()
 goto(0, 100)
-color(colours['reallyraspberry'])
+color(cores['raspberry'])
 style = ('Arial', 40, 'bold')
-write('HELLO', font=style, align='center')
+write('OLÁ,', font=style, align='center')
 right(90)
 forward(60)
-color(colours['awesomeorange'])
-write('WORLD', font=style, align='center')
+color(cores['laranjaforte'])
+write('MUNDO', font=style, align='center')
 hideturtle()
 
 sleep(3)
 
 goto(0,0)
-color(colours['moongrey'])
+color(cores['cinzalua'])
 dot(350)
 
 setheading(-90)
 penup()
 hideturtle()
 goto(0, 135)
-color(colours['verylime'])
+color(cores['verdelimao'])
 style=('Verdana', 20, 'bold')
-write('A typical', font=style, align='center')
+write('Um típico', font=style, align='center')
 forward(40)
-color(colours['reallyraspberry'])
-write('smart phone', font=('Verdana', 25, 'bold'), align='center')
+color(cores['raspberry'])
+write('telefone celular', font=('Verdana', 22, 'bold'), align='center')
 forward(40)
-color(colours['deepsea'])
-write('has more', font=('Verdana', 18, 'bold'), align='center')
+color(cores['fundodomar'])
+write('possui mais', font=('Verdana', 18, 'bold'), align='center')
 forward(40)
-color(colours['awesomeorange'])
-write('computing power', font=('Verdana', 25, 'bold'), align='center')
+color (cores ['laranjaforte'])
+write('poder de computação', font=('Verdana', 21, 'bold'), align='center')
 forward(40)
-color(colours['perfectpurple'])
-write('than', font=style, align='center')
+color(cores['roxodahora'])
+write('que', font=style, align='center')
 forward(40)
-color(colours['coolcyan'])
-write('Apollo 11', font=('Verdana', 25, 'bold'), align='center')
-color(colours['lovelylemon'])
+color(cores['azulmaneiro'])
+write('a Apollo 11', font=('Verdana', 25, 'bold'), align='center')
+color(cores['limaobacana'])
 forward(40)
-write('when it landed on', font=style, align='center')
-color(colours['gloomygrey'])
+write('quando pousou', font=style, align='center')
+color(cores['cinzaescuro'])
 forward(40)
-write('the moon', font=('Verdana', 25, 'bold'), align='center')
+write('na Lua', font=('Verdana', 25, 'bold'), align='center')
 color('white')
 forward(50)
 write('- Nancy Gibbs, 2012', font=('Arial', 14, 'normal'))
