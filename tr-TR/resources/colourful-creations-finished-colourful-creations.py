4baa5cd1658b673424cf1a0068507641cbc63a39
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2020-06-16 17:02:48 +0100

    New translations colourful-creations-finished-colourful-creations.py (Turkish)

--- tr-TR/resources/colourful-creations-finished-colourful-creations.py
+++ tr-TR/resources/colourful-creations-finished-colourful-creations.py
@@ -3,70 +3,70 @@
 from turtle import *
 from time import *
 
-# introduce dictionaries
-# use a colour picker to find and choose new colours
-colours = { 
-  'space': '#060608', 
-  'moongrey': '#BCBDEF', 
-  'verylime': '#A7E30E', 
-  'deepsea': '#226363', 
-  'reallyraspberry': '#FA057F', 
-  'gloomygrey': '#363332', 
-  'awesomeorange':  '#F37C06', 
-  'coolcyan': '#4FEEF6',
-  'perfectpurple': '#6820B0',
-  'lovelylemon': '#FBF312',
+# sözlükleri tanıtma
+# yeni renkler bulmak ve seçmek için bir renk seçici kullanın
+renkler = { 
+  'uzay': '#060608', 
+  'aygrisi': '#BCBDEF', 
+  'limonata': '#A7E30E', 
+  'derindeniz': '#226363', 
+  'gercekahududu': '#FA057F', 
+  'kasvetligri': '#363332', 
+  'muhtesemturuncu':  '#F37C06', 
+  'karizmatikcamgobegi': '#4FEEF6',
+  'mukemmelmor': '#6820B0',
+  'sarimtrak': '#FBF312',
 }
 
-screen = Screen()
-screen.setup(400, 400)
-screen.bgcolor(colours['space'])
+ekran = Screen()
+ekran.setup(400, 400)
+ekran.bgcolor(renkler['uzay'])
 
 penup()
 goto(0, 100)
-color(colours['reallyraspberry'])
+color(renkler['gercekahududu'])
 style = ('Arial', 40, 'bold')
-write('HELLO', font=style, align='center')
+write('MERHABA', font=style, align='center')
 right(90)
 forward(60)
-color(colours['awesomeorange'])
-write('WORLD', font=style, align='center')
+color(renkler['muhtesemturuncu'])
+write('DÜNYA', font=style, align='center')
 hideturtle()
 
 sleep(3)
 
 goto(0,0)
-color(colours['moongrey'])
+color(renkler['aygrisi'])
 dot(350)
 
 setheading(-90)
 penup()
 hideturtle()
 goto(0, 135)
-color(colours['verylime'])
+color(renkler['limonata'])
 style=('Verdana', 20, 'bold')
-write('A typical', font=style, align='center')
+write('Ortalama', font=style, align='center')
 forward(40)
-color(colours['reallyraspberry'])
-write('smart phone', font=('Verdana', 25, 'bold'), align='center')
+color(renkler['gercekahududu'])
+write('bir akıllı', font=('Verdana', 25, 'bold'), align='center')
 forward(40)
-color(colours['deepsea'])
-write('has more', font=('Verdana', 18, 'bold'), align='center')
+color(renkler['derindeniz'])
+write('telefonda', font=('Verdana', 18, 'bold'), align='center')
 forward(40)
-color(colours['awesomeorange'])
-write('computing power', font=('Verdana', 25, 'bold'), align='center')
+color(renkler['muhtesemturuncu'])
+write('Aya iniş', font=('Verdana', 25, 'bold'), align='center')
 forward(40)
-color(colours['perfectpurple'])
-write('than', font=style, align='center')
+color(renkler['mukemmelmor'])
+write('yapan', font=style, align='center')
 forward(40)
-color(colours['coolcyan'])
-write('Apollo 11', font=('Verdana', 25, 'bold'), align='center')
-color(colours['lovelylemon'])
+color(renkler['karizmatikcamgobegi'])
+write('Apollo 11\'den', font=('Verdana', 25, 'bold'), align='center')
+color(renkler['sarimtrak'])
 forward(40)
-write('when it landed on', font=style, align='center')
-color(colours['gloomygrey'])
+write('daha fazla hesaplama', font=style, align='center')
+color(renkler['kasvetligri'])
 forward(40)
-write('the moon', font=('Verdana', 25, 'bold'), align='center')
+write('gücü vardır', font=('Verdana', 25, 'bold'), align='center')
 color('white')
 forward(50)
 write('- Nancy Gibbs, 2012', font=('Arial', 14, 'normal'))
