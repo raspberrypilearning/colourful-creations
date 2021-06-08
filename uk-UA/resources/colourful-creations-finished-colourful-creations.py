bd2c38c09aa99fc31de80ea82191a85173050d7f
majamanojlovic <49232422+majamanojlovic@users.noreply.github.com>
2021-06-08 12:48:22 +0100

    New translations colourful-creations-finished-colourful-creations.py (Ukrainian)

--- uk-UA/resources/colourful-creations-finished-colourful-creations.py
+++ uk-UA/resources/colourful-creations-finished-colourful-creations.py
@@ -3,70 +3,70 @@
 from turtle import *
 from time import *
 
-# introduce dictionaries
-# use a colour picker to find and choose new colours
+# представляємо словники
+# використовуй інструмент підбору кольорів для пошуку та вибору нових кольорів
 colours = { 
-  'space': '#060608', 
-  'moongrey': '#BCBDEF', 
-  'verylime': '#A7E30E', 
-  'deepsea': '#226363', 
-  'reallyraspberry': '#FA057F', 
-  'gloomygrey': '#363332', 
-  'awesomeorange':  '#F37C06', 
-  'coolcyan': '#4FEEF6',
-  'perfectpurple': '#6820B0',
-  'lovelylemon': '#FBF312',
+  'космос': '#060608', 
+  'місячно_сірий': '#BCBDEF', 
+  'дуже_лимонний': '#A7E30E', 
+  'глибоко_морський': '#226363', 
+  'реально_малиновий': '#FA057F', 
+  'хмуро_сірий': '#363332', 
+  'неймовірно_апельсиновий': '#F37C06', 
+  'круто_блакитний': '#4FEEF6',
+  'ідеально_пурпурний': '#6820B0',
+  'чудово_лимонний': '#FBF312',
 }
 
 screen = Screen()
 screen.setup(400, 400)
-screen.bgcolor(colours['space'])
+screen.bgcolor(colours['космос'])
 
 penup()
 goto(0, 100)
-color(colours['reallyraspberry'])
+color(colours['реально_малиновий'])
 style = ('Arial', 40, 'bold')
-write('HELLO', font=style, align='center')
+write('Привіт', font = style, align = 'center')
 right(90)
 forward(60)
-color(colours['awesomeorange'])
-write('WORLD', font=style, align='center')
+color(colours['неймовірно_апельсиновий'])
+write('Світ', font = style, align = 'center')
 hideturtle()
 
 sleep(3)
 
 goto(0,0)
-color(colours['moongrey'])
+color(colours['місячно_сірий'])
 dot(350)
 
 setheading(-90)
 penup()
 hideturtle()
 goto(0, 135)
-color(colours['verylime'])
-style=('Verdana', 20, 'bold')
-write('A typical', font=style, align='center')
+color(colours['дуже_лимонний'])
+style = ('Verdana', 15, 'bold')
+write('Типовий', font = style, align = 'center')
 forward(40)
-color(colours['reallyraspberry'])
-write('smart phone', font=('Verdana', 25, 'bold'), align='center')
+color(colours['реально_малиновий'])
+write('смартфон', font = ('Verdana', 25, 'bold'), align = 'center')
 forward(40)
-color(colours['deepsea'])
-write('has more', font=('Verdana', 18, 'bold'), align='center')
+color(colours['глибоко_морський'])
+write('має більше', font = ('Verdana', 18, 'bold'), align = 'center')
 forward(40)
-color(colours['awesomeorange'])
-write('computing power', font=('Verdana', 25, 'bold'), align='center')
+color(colours['неймовірно_апельсиновий'])
+write('обчислювальної потужності', font = ('Verdana', 15, 'bold'), align = 'center')
 forward(40)
-color(colours['perfectpurple'])
-write('than', font=style, align='center')
+color(colours['ідеально_пурпурний'])
+write('ніж', font = style, align = 'center')
 forward(40)
-color(colours['coolcyan'])
-write('Apollo 11', font=('Verdana', 25, 'bold'), align='center')
-color(colours['lovelylemon'])
+color(colours['круто_блакитний'])
+write('Аполлон 11', font = ('Verdana', 25, 'bold'), align = 'center')
+color(colours['чудово_лимонний'])
 forward(40)
-write('when it landed on', font=style, align='center')
-color(colours['gloomygrey'])
+write('коли він висадився', font = style, align = 'center')
+color(colours['хмуро_сірий'])
 forward(40)
-write('the moon', font=('Verdana', 25, 'bold'), align='center')
+write('на Місяці', font = ('Verdana', 25, 'bold'), align = 'center')
 color('white')
 forward(50)
-write('- Nancy Gibbs, 2012', font=('Arial', 14, 'normal'))
+write('- Ненсі Гіббс, 2012', font = ('Arial', 14, 'normal'))
